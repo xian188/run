09540cb3bc758317076d0a48ab4e183fede20d24
xian188 <127485554+xian188@users.noreply.github.com>
2023-06-12 08:08:49 +0100

    Create forge.config.js

---
+++ forge.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    packagerConfig: {
+        name: 'Cursor',
+        icon: 'assets/icon/icon',
+        extraResource: ['./lsp', './resources', './tutor'],
+        osxSign: {},
+    },
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin'],
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {
+                options: {
+                    icon: 'assets/icon/icon.png',
+                },
+            },
+        },
+    ],
+    plugins: [
+        {
+            name: '@electron-forge/plugin-webpack',
+            config: {
+                mainConfig: './webpack.main.config.js',
+                devContentSecurityPolicy:
+                    "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';",
+                renderer: {
+                    config: './webpack.renderer.config.js',
+                    entryPoints: [
+                        {
+                            html: './src/index.html',
+                            js: './src/index.ts',
+                            name: 'main_window',
+                            preload: {
+                                js: './src/preload.ts',
+                            },
+                        },
+                    ],
+                },
+            },
+        },
+    ],
+}
